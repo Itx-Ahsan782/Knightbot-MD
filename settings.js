f07df35b4553db6c163318ca332003e1bdf3ed45
Itx-Ahsan782 <ja0878907@gmail.com>
2025-03-24 13:05:50 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  author: 'king',
+  botName: "Ahsan king",
+  botOwner: 'Ahsan king', // Your name
+  ownerNumber: '923706302550', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a  for managing group commands and automating tasks.",
   version: "1.0.0",
 };
 
